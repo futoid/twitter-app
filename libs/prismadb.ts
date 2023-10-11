e70ce2fb449f8dccf2fe00dca347a6fccc8049f5
futoid <aliekmandal24@gmail.com>
2023-10-11 18:24:01 +0530

    prisma fix

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-declare global{
-    var prisma: PrismaClient | undefined
+
+declare global {
+	var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
-if(process.env.NODE_ENV != 'production') globalThis.prisma = client;
+const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
-export default client;
+export default prisma;
